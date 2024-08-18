ff57b689a938760c0eeb9083e6f3661a8c93b511
mehrani <mehraniali8002@gmail.com>
2024-08-18 21:09:02 +0330

    add another testcase

--- src/RtC/src/main/java/org/example/Main.java
+++ src/RtC/src/main/java/org/example/Main.java
@@ -22,19 +22,17 @@ public class Main {
         System.out.println(automatonUtils.transitionsIntersectionExists(new ArrayList<>(List.of("a", "b")), new ArrayList<>(List.of("b", "d"))));
 
         try {
-            ConstraintAutomaton automaton_1 = mapper.readValue(new File("src/main/resources/testcases/3/automaton-1.json"), ConstraintAutomaton.class);
-            ConstraintAutomaton automaton_2 = mapper.readValue(new File("src/main/resources/testcases/3/automaton-2.json"), ConstraintAutomaton.class);
-            ConstraintAutomaton automaton_3 = mapper.readValue(new File("src/main/resources/testcases/3/automaton-3.json"), ConstraintAutomaton.class);
+            ConstraintAutomaton automaton_1 = mapper.readValue(new File("src/main/resources/testcases/4/automaton-1.json"), ConstraintAutomaton.class);
+            ConstraintAutomaton automaton_2 = mapper.readValue(new File("src/main/resources/testcases/4/automaton-2.json"), ConstraintAutomaton.class);
+//            ConstraintAutomaton automaton_3 = mapper.readValue(new File("src/main/resources/testcases/3/automaton-3.json"), ConstraintAutomaton.class);
 
-//            ConstraintAutomaton joinedAutomaton = automataOps.joinAutomata(automaton_1, automaton_2);
-//            ConstraintAutomaton finalAutomaton = automataOps.joinAutomata(joinedAutomaton, automaton_3);
+            ConstraintAutomaton joinedAutomaton = automataOps.joinAutomata(automaton_1, automaton_2);
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/main/resources/testcases/4/result.json"), joinedAutomaton);
 
-//            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/main/resources/testcases/3/result.json"), finalAutomaton);
-
-            ConstraintAutomaton joinedAutomaton_2 = automataOps.joinAutomata(automaton_3, automaton_1);
-            ConstraintAutomaton finalAutomaton_2 = automataOps.joinAutomata(joinedAutomaton_2, automaton_2);
-
-            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/main/resources/testcases/3/result-2.json"), finalAutomaton_2);
+//            ConstraintAutomaton joinedAutomaton_2 = automataOps.joinAutomata(automaton_3, automaton_1);
+//            ConstraintAutomaton finalAutomaton_2 = automataOps.joinAutomata(joinedAutomaton_2, automaton_2);
+//
+//            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/main/resources/testcases/3/result-2.json"), finalAutomaton_2);
 
         } catch (IOException e) {
             e.printStackTrace();
