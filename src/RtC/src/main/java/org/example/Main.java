218e6835d7f8e751c4440dbef9a8aa4eb3c42ee7
mehrani <mehraniali8002@gmail.com>
2024-08-22 14:51:36 +0330

    add regex for detecting all automaton files in the testcase directory

--- src/RtC/src/main/java/org/example/Main.java
+++ src/RtC/src/main/java/org/example/Main.java
@@ -14,28 +14,28 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         ObjectMapper mapper = new ObjectMapper();
         FileUtils fileUtils = new FileUtils(mapper);
         AutomatonUtils automatonUtils = new AutomatonUtils();
         SingleJoin automataOps = new SingleJoin(automatonUtils);
         MultiJoin multiJoin = new MultiJoin(automataOps, fileUtils);
-        System.out.println(automatonUtils.transitionsIntersectionExists(new ArrayList<>(List.of("a", "b")), new ArrayList<>(List.of("b", "d"))));
 
         try {
-            ConstraintAutomaton automaton_1 = fileUtils.readAutomatonFromFile("src/main/resources/testcases/7/automaton-1.json");
-            ConstraintAutomaton automaton_2 = fileUtils.readAutomatonFromFile("src/main/resources/testcases/7/automaton-2.json");
-            ConstraintAutomaton automaton_3 = fileUtils.readAutomatonFromFile("src/main/resources/testcases/7/automaton-3.json");
-            ConstraintAutomaton automaton_4 = fileUtils.readAutomatonFromFile("src/main/resources/testcases/7/automaton-4.json");
+            ArrayList<ConstraintAutomaton> automatonList = fileUtils.readConstraintAutomataFromTestcases("src/main/resources/testcases/5/");
+
+//            ConstraintAutomaton automaton_1 = fileUtils.readAutomatonFromFile("src/main/resources/testcases/7/automaton-1.json");
+//            ConstraintAutomaton automaton_2 = fileUtils.readAutomatonFromFile("src/main/resources/testcases/7/automaton-2.json");
+//            ConstraintAutomaton automaton_3 = fileUtils.readAutomatonFromFile("src/main/resources/testcases/7/automaton-3.json");
+//            ConstraintAutomaton automaton_4 = fileUtils.readAutomatonFromFile("src/main/resources/testcases/7/automaton-4.json");
 
 //            ArrayList<ConstraintAutomaton> automatonList = new ArrayList<>(List.of(automaton_1, automaton_2, automaton_3, automaton_4));
-            ArrayList<ConstraintAutomaton> automatonList = new ArrayList<>(List.of(automaton_3, automaton_4, automaton_2, automaton_1));
+//            ArrayList<ConstraintAutomaton> automatonList = new ArrayList<>(List.of(automaton_3, automaton_4, automaton_2, automaton_1));
 
 
-            ConstraintAutomaton result = multiJoin.joinWithNoHeuristic(automatonList);
-            fileUtils.writeAutomatonToFile("src/main/resources/testcases/7/result-order-2.json", result);
+//            ConstraintAutomaton result = multiJoin.joinWithNoHeuristic(automatonList);
+//            fileUtils.writeAutomatonToFile("src/main/resources/testcases/7/result-order-2.json", result);
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/RtC/src/main/java/org/example/utils/FileUtils.java
+++ src/RtC/src/main/java/org/example/utils/FileUtils.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FileUtils {
     private final ObjectMapper objectMapper;
@@ -19,8 +20,27 @@ public class FileUtils {
     public ArrayList<ConstraintAutomaton> readConstraintAutomataFromTestcases(String directory) {
         ArrayList<ConstraintAutomaton> automataList = new ArrayList<>();
 
-        //todo
-
+        File dir = new File(directory);
+        if (dir.exists() && dir.isDirectory()) {
+            File[] files = dir.listFiles((d, name) -> name.matches("automaton-\\d+\\.json"));
+            if (files != null) {
+                Arrays.sort(files, (f1, f2) -> {
+                    int num1 = Integer.parseInt(f1.getName().replaceAll("\\D+", ""));
+                    int num2 = Integer.parseInt(f2.getName().replaceAll("\\D+", ""));
+                    System.out.println(num1 + ", " + num2);
+                    return Integer.compare(num1, num2);
+                });
+                for (File file : files) {
+                    System.out.print(file.getName() + ", ");
+                    try {
+                        ConstraintAutomaton automaton = objectMapper.readValue(file, ConstraintAutomaton.class);
+                        automataList.add(automaton);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
         return automataList;
     }
 
