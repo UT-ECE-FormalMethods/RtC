b01030c8b7f920254a51ef55a73d1f82904d75fa
mehrani <mehraniali8002@gmail.com>
2024-12-15 14:38:33 +0330

    add run script

---
+++ RtC/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# defining the Java executable path
+JAVA_EXEC="C:\Program Files\Java\jdk-19\bin\java.exe"
+
+# defining the classpath that appears in IntelliJ IDEA run config
+CLASSPATH="path\to\target\classes;path\to\fasterxml\jackson\core\jackson-databind\2.16.1\jackson-databind-2.16.1.jar;path\to\fasterxml\jackson\core\jackson-annotations\2.16.1\jackson-annotations-2.16.1.jar;path\to\fasterxml\jackson\core\jackson-core\2.16.1\jackson-core-2.16.1.jar"
+
+# defining the main class to run
+MAIN_CLASS="org.example.Main"
+
+# executing the program for 10 times to accurately measure the execution time
+for i in {1..10}
+do
+    "$JAVA_EXEC" -classpath "$CLASSPATH" "$MAIN_CLASS"
+    echo "Execution $i completed."
+done
+
+echo "All 10 executions completed."
--- RtC/src/main/java/org/example/Main.java
+++ RtC/src/main/java/org/example/Main.java
@@ -21,8 +21,8 @@ public class Main {
         HeuristicUtils heuristicUtils = new HeuristicUtils(automatonUtils);
         SingleJoin automataOps = new SingleJoin(automatonUtils);
         MultiJoin multiJoin = new MultiJoin(automataOps, fileUtils, heuristicUtils);
-        String testcaseDirectoryName = "27";
-        int heuristicType = 9;
+        String testcaseDirectoryName = "7";
+        int heuristicType = 8;
 
         System.out.println("testcase : " + testcaseDirectoryName + ", heuristic type: " + heuristicType);
         try {
