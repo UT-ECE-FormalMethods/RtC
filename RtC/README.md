de613feac588aeaf8e303cefa73e0933fa1847bb
mehrani <mehraniali8002@gmail.com>
2024-12-30 17:10:54 +0330

    add inital analysis script

---
+++ RtC/README.md
@@ -0,0 +1,3 @@
+execute `run.sh` to rerun the program as many times as needed (no caching occurs in this case).
+
+Note: the `run.sh` assumes that the required packages(managed by Maven) are already installed and no further installation is required.
---
+++ analysis/main.py
@@ -0,0 +1,56 @@
+import json
+import matplotlib.pyplot as plt
+
+
+def read_data(json_file):
+    with open(json_file, "r") as file:
+        return json.load(file)
+
+def generate_data(data, ignore_max_connectivity):
+    heuristic_names = []
+    heuristic_avg_times = {}
+
+    for testcase in data["results"]:
+        testcase_id = int(testcase["testcase_id_to_show"])
+        for heuristic in testcase["heuristics"]:
+            name = heuristic["name"]
+            avg_time = (heuristic["avg_time"]/1000) #converting to seconds
+
+            if name == "max_connectivity" and ignore_max_connectivity:
+                continue
+
+            if name not in heuristic_names:
+                heuristic_names.append(name)
+                heuristic_avg_times[name] = []
+
+            heuristic_avg_times[name].append((testcase_id, avg_time))
+    
+    return heuristic_avg_times
+
+
+def plot_data(heuristic_avg_times, fig_id):
+    for heuristic in heuristic_avg_times:
+        heuristic_avg_times[heuristic].sort(key=lambda x: x[0])
+
+    plt.figure(figsize=(10, 6))
+    for heuristic in heuristic_avg_times:
+        x = [item[0] for item in heuristic_avg_times[heuristic]]
+        y = [item[1] for item in heuristic_avg_times[heuristic]]
+        plt.plot(x, y, marker='o', label=heuristic)
+
+    plt.xlabel("Testcase ID", fontsize=12)
+    plt.ylabel("Average Time (ms)", fontsize=12)
+    plt.title(f"Performance of Heuristics - figure {fig_id}", fontsize=14)
+    plt.legend(title="Heuristics", fontsize=10)
+    plt.grid(alpha=0.4)
+
+    plt.tight_layout()
+    plt.show()
+
+
+data = read_data('results.json')
+heuristic_data = generate_data(data, False)
+plot_data(heuristic_data, 1)
+
+heuristic_data = generate_data(data, True)
+plot_data(heuristic_data, 2)
