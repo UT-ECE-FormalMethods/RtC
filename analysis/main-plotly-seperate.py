4776c757c30cae4d6bfd9218022441e02bed4126
mehrani <mehraniali8002@gmail.com>
2025-02-03 15:29:08 +0330

    complete testcases section

--- analysis/main-plotly-seperate.py
+++ analysis/main-plotly-seperate.py
@@ -1,6 +1,5 @@
 import json
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 def read_data(json_file):
     with open(json_file, "r") as file:
@@ -36,22 +35,22 @@ heuristic_colors = {
     "max_connectivity": "gray"
 }
 
-def plot_data_plotly(heuristic_avg_times, input_size_str, row, col, fig, show_legend):
-    time_unit = "s"  # Default time unit
-    if input_size_str == "Large":
-        time_unit = "min"  # Change to minutes for the "Large" subplot
+def plot_data_plotly(heuristic_avg_times, input_size_str):
+    time_unit = "s" if input_size_str != "Large" else "min"
+    fig = go.Figure()
     for heuristic in heuristic_avg_times:
         heuristic_avg_times[heuristic].sort(key=lambda x: x[0])
-        x = [item[0] for item in heuristic_avg_times[heuristic]]
+        x = [input_size_str[0] + "-" + str(item[0]) for item in heuristic_avg_times[heuristic]]
         y = [item[1] for item in heuristic_avg_times[heuristic]]
         fig.add_trace(go.Scatter(
             x=x, y=y, mode='lines+markers',
             name=heuristic,
-            marker_color=heuristic_colors[heuristic],
-            showlegend=show_legend  # Control legend visibility based on input parameter
-        ), row=row, col=col)
-    fig.update_xaxes(title_text="Testcase ID", row=row, col=col, type='category')
-    fig.update_yaxes(title_text=f"Average Time ({time_unit})", row=row, col=col)
+            marker_color=heuristic_colors.get(heuristic, "black")
+        ))
+    fig.update_xaxes(title_text="Testcase ID", type='category')
+    fig.update_yaxes(title_text=f"Average Time ({time_unit})")
+    fig.update_layout(title_text=f"Heuristic Performance - {input_size_str}", height=800, width=1000)
+    fig.show()
 
 # Load Data
 data_small = read_data('results-small.json')
@@ -63,15 +62,7 @@ heuristic_data_small = generate_data(data_small, False, False)
 heuristic_data_medium = generate_data(data_medium, False, False)
 heuristic_data_large = generate_data(data_large, False, True)
 
-fig = make_subplots(rows=2, cols=2, subplot_titles=("Small", "Medium", "Large", ""))
-
-# Call plot_data_plotly for each dataset, controlling legend visibility
-plot_data_plotly(heuristic_data_small, "Small", 1, 1, fig, show_legend=False)
-plot_data_plotly(heuristic_data_medium, "Medium", 1, 2, fig, show_legend=False)
-plot_data_plotly(heuristic_data_large, "Large", 2, 1, fig, show_legend=True)
-
-# Update layout
-fig.update_layout(height=1500, width=1500, title_text="Heuristic Performance Comparison")
-
-# Show the figure
-fig.show()
+# Generate separate plots
+plot_data_plotly(heuristic_data_small, "Small")
+plot_data_plotly(heuristic_data_medium, "Medium")
+plot_data_plotly(heuristic_data_large, "Large")
